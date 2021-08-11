4b5340f3605d7c20812e79359c4cecf61b684f82
Wyrmstooth <88787797+Wyrmstooth@users.noreply.github.com>
2021-08-11 20:32:51 +0500

    Update script.js

--- script.js
+++ script.js
@@ -50,10 +50,12 @@ document.querySelector('.check').addEventListener('click', function () {
 document.querySelector('.again').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
+    document.querySelector('.score').textContent = '20'
   document.querySelector('.message').textContent = 'Start Guessing...';
   document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
+   document.querySelector('.number').textContent = '?'
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
 });
